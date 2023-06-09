590f3522a78adf424225cfccedb11dc5bb7dd65a
haider-patanwala <haiderpatanwala@hotmail.com>
2023-06-10 04:22:44 +0530

    Created Single Products Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
 import "./App.css"
 import Header from "./components/Header"
 import Home from "./modules/Home"
+import Product from "./modules/Home/Product"
+import Footer from "./components/Footer"
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Home />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/products/:id' element={<Product />} />
+			</Routes>
+			<Footer />
 		</div>
 	)
 }
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -6,23 +6,24 @@ const Products = ({ products = [] }) => {
 			<div className='container px-5 py-24 mx-auto'>
 				<div className='flex flex-wrap -m-4'>
 					{products.map((product) => {
+						const { id, title, price, description, category, image } = product
 						return (
-							<div className='lg:w-1/4 md:w-1/2 p-4 w-full'>
+							<div className='lg:w-1/4 md:w-1/2 p-4 w-full mb-2 cursor-pointer'>
 								<a className='block relative h-48 rounded overflow-hidden'>
 									<img
-										alt='ecommerce'
-										className='object-cover object-center w-full h-full block'
-										src='https://dummyimage.com/420x260'
+										alt={title}
+										className='object-contain object-center w-full h-full block'
+										src={image}
 									/>
 								</a>
 								<div className='mt-4'>
-									<h3 className='text-gray-500 text-xs tracking-widest title-font mb-1'>
-										CATEGORY
+									<h3 className='text-gray-500 text-xs tracking-widest title-font mb-1 uppercase'>
+										{category}
 									</h3>
 									<h2 className='text-gray-900 title-font text-lg font-medium'>
-										The Catalyzer
+										{title}
 									</h2>
-									<p className='mt-1'>$16.00</p>
+									<p className='mt-1 font-semibold text-md'>₹{price}</p>
 								</div>
 							</div>
 						)
